ada72214b61d4865b2fc01ba3cea40defb5c04b9
Andy Bulka <abulka@gmail.com>
2021-07-17 11:36:10 +1000

    flutter flow

---
+++ lib/flutter_flow_example1.dart
@@ -0,0 +1,95 @@
+// import '../flutter_flow/flutter_flow_theme.dart';
+// import '../flutter_flow/flutter_flow_util.dart';
+// import '../flutter_flow/flutter_flow_widgets.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// class AbsolutistWidget extends StatefulWidget {
+//   AbsolutistWidget({Key key}) : super(key: key);
+
+//   @override
+//   _AbsolutistWidgetState createState() => _AbsolutistWidgetState();
+// }
+
+// class _AbsolutistWidgetState extends State<AbsolutistWidget> {
+//   final scaffoldKey = GlobalKey<ScaffoldState>();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       key: scaffoldKey,
+//       body: SafeArea(
+//         child: Stack(
+//           children: [
+//             Align(
+//               alignment: Alignment(-0.84, -0.71),
+//               child: ElevatedButton(
+//                 onPressed: () {
+//                   print('Button pressed ...');
+//                 },
+//                 child: Text('Button'),
+//                 // text: 'Button',
+//                 // options: FFButtonOptions(
+//                 //   width: 130,
+//                 //   height: 40,
+//                 //   color: FlutterFlowTheme.primaryColor,
+//                 //   textStyle: FlutterFlowTheme.subtitle2.override(
+//                 //     fontFamily: 'Poppins',
+//                 //     color: Colors.white,
+//                 //   ),
+//                 //   borderSide: BorderSide(
+//                 //     color: Colors.transparent,
+//                 //     width: 1,
+//                 //   ),
+//                 //   borderRadius: 12,
+//                 // ),
+//               ),
+//             ),
+//             Align(
+//               alignment: Alignment(0.43, -0.68),
+//               child: Text(
+//                 'Hello World',
+//                 style: FlutterFlowTheme.bodyText1.override(
+//                   fontFamily: 'Poppins',
+//                 ),
+//               ),
+//             ),
+//             Align(
+//               alignment: Alignment(-0.8, -0.22),
+//               child: FFButtonWidget(
+//                 onPressed: () {
+//                   print('Button pressed ...');
+//                 },
+//                 text: 'Button',
+//                 options: FFButtonOptions(
+//                   width: 130,
+//                   height: 40,
+//                   color: FlutterFlowTheme.primaryColor,
+//                   textStyle: FlutterFlowTheme.subtitle2.override(
+//                     fontFamily: 'Poppins',
+//                     color: Colors.white,
+//                   ),
+//                   borderSide: BorderSide(
+//                     color: Colors.transparent,
+//                     width: 1,
+//                   ),
+//                   borderRadius: 12,
+//                 ),
+//               ),
+//             ),
+//             Align(
+//               alignment: Alignment(0.71, -0.23),
+//               child: Text(
+//                 'Hello World',
+//                 style: FlutterFlowTheme.bodyText1.override(
+//                   fontFamily: 'Poppins',
+//                   fontSize: 24,
+//                 ),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
